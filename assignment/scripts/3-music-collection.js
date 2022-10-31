5915713751d74675d7a199a417f4c3bf11bc098e
Liam King <techy.willyk3@gmail.com>
2022-10-31 18:32:07 -0500

    Got my object working when i called it input in the devtool.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,5 +1,12 @@
 console.log('***** Music Collection *****')
+
+const musicFavs = {
+    title: 'Libro',
+    artist: 'Mimi',
+    yearPublished: 1998
+};
+
 let collection = [];
-function addToCollection(){
+function addToCollection( title, artist, yearPublished ){
     
 }
