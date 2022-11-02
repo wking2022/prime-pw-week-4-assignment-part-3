6ae6d501b01d58b4f4cab914da30acc8ec0a20aa
Liam King <techy.willyk3@gmail.com>
2022-11-02 17:16:50 -0500

    Created a new object and pushed it to the colloection array.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,12 +1,22 @@
 console.log('***** Music Collection *****')
 
-const musicFavs = {
+let collection = [];
+let albums = {
     title: 'Libro',
     artist: 'Mimi',
     yearPublished: 1998
+}
+let newObj = {
+    title: 'Tell Me Again',
+    artist: 'Me',
+    yearPublished: 1990
+}
+collection.push(newObj);
+
+function addToCollection( title, artist, yearPublished ){
 };
 
-let collection = [];
-function addToCollection( title, artist, yearPublished ){
-    
-}
+
+// addToCollection.push[i]('Harry Potter', 'Bel Viv Duvoh', 1962);
+
+// console.log( 'Here is what is in my first object:', albums);
