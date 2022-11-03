9b8aaf6312e55fbfe6f2160dde5e6a2659fd4571
Liam King <techy.willyk3@gmail.com>
2022-11-02 23:13:05 -0500

    Figured out a bit less complicated way of getting it done. Feeling good.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -10,33 +10,7 @@ let collection = [];
 // }
 // let albums1 = {
 //     title: 'Must Be', artist: 'Good Sight Will', yearPublished: 2021
-// }
-// let albums2 = {
-//     title: 'Blessed Up',
-//     artist: 'Good Sight Will',
-//     yearPublished: 2022
-// }
-// let albums3  = {
-//     title: 'Must Be',
-//     artist: 'Good Sight Will',
-//     yearPublished: 2019
-// }
-// let albums4  = {
-//     title: 'Red Wine',
-//     artist: 'Bob Marley',
-//     yearPublished: 1983
-// }
-// let albums5 = {
-//     title: 'Those Old Days',
-//     artist: 'Willy Killington',
-//     yearPublished: 1983
-// }
-// let albums6 = {
-//     title: 'Weak Link',
-//     artist: 'Kingdom Bill',
-//     yearPublished: 2001
-// }
-
+//
 
 function addToCollection( title, artist, yearPublished ){
     let albums = {
@@ -44,18 +18,17 @@ function addToCollection( title, artist, yearPublished ){
     artist: artist,
     yearPublished: yearPublished
     }
-    
     return collection.push([title, artist, yearPublished]);
 };
-function showCollection([]){
-    return
-}
 
+
+addToCollection('Weak Link','Kingdom Bill', 2001);
+console.log(collection);
 addToCollection('Must Be','Good Sight Will', 2021);
 console.log(collection);
 
 
-console.log('Here is a list of some of my favorite Albums:', collection);
+// console.log('Here is a list of some of my favorite Albums:', collection);
 
 
 // albums.addToCollection( 'All about Me', 'Me', 2001 );
@@ -64,3 +37,7 @@ console.log('Here is a list of some of my favorite Albums:', collection);
 // addToCollection.push[i]('Harry Potter', 'Bel Viv Duvoh', 1962);
 
 // console.log( 'Here is what is in my first object:', albums);
+
+// function showCollection([]){
+//     return
+// }
